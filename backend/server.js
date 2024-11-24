05dc58bf4715ab6723b57e549dab2a65dcb32021
MaiHien <hienmtk.info2022@gmail.com>
2024-11-24 10:54:18 +0700

    done

--- backend/server.js
+++ backend/server.js
@@ -15,10 +15,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-// app.use(cors({
-//     origin: 'http://localhost:3000',
-//     credentials: true
-// }));
 app.use(session({
     secret: '123456',
     resave: false,
